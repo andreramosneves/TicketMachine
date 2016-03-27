97f6f84687525707ea42c9cadf1e87edbed74c54
denis <denis@169.254.110.28>
2016-03-27 19:58:33 -0300

    adicionado mensagem de erro no classe papelmoedaInvalidaException

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,12 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
+
+    public PapelMoedaInvalidaException() {
+      super("PAPEL MOEDA INVALIDO");
+      
+    }
+    
+    
     
 }
